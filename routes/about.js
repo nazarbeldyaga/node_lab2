689486afbfd5f2b64bb78e00a5a3d669d32bf762
Diana Bryzhitska <bryzhitskaaa>
2025-02-18 01:02:57 +0200

    Fix: update image paths in about.js

--- routes/about.js
+++ routes/about.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const people = {
-    nazar: { name: "Назар", description: "Опис Назара", photo: "../public/stylesheets/images/nazar.jpg" },
-    illya: { name: "Ілля", description: "Опис Іллі", photo: "../public/stylesheets/images/illya.jpg" },
-    diana: { name: "Діана", description: "Опис Діани", photo: "../public/stylesheets/images/diana.jpg" },
-    vanya: { name: "Ваня", description: "Опис Вані (Івана, якщо повністю)", photo: "../public/stylesheets/images/vanya.jpg" },
-    esfer: { name: "Есфер", description: "Опис Еськи", photo: "../public/stylesheets/images/esfer.jpg" }
+    nazar: { name: "Назар", description: "Опис Назара", photo: "/stylesheets/images/nazar.jpg" },
+    illya: { name: "Ілля", description: "Опис Іллі", photo: "/stylesheets/images/illya.jpg" },
+    diana: { name: "Діана", description: "Опис Діани", photo: "/stylesheets/images/diana.jpg" },
+    vanya: { name: "Ваня", description: "Опис Вані (Івана, якщо повністю)", photo: "/stylesheets/images/vanya.jpg" },
+    esfer: { name: "Есфер", description: "Опис Еськи", photo: "/stylesheets/images/esfer.jpg" }
 };
 
 router.get('/:name', function(req, res) {
