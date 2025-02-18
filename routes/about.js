37f5a84c88f8b0602a65820d6aa00e57b2727109
Diana Bryzhitska <bryzhitskaaa>
2025-02-18 14:10:30 +0200

    Added team photos and changed descriptions

--- routes/about.js
+++ routes/about.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const people = {
-    nazar: { name: "Назар", description: "Опис Назара", photo: "/stylesheets/images/nazar.jpg" },
-    illya: { name: "Ілля", description: "Опис Іллі", photo: "/stylesheets/images/illya.jpg" },
-    diana: { name: "Діана", description: "Опис Діани", photo: "/stylesheets/images/diana.jpg" },
-    vanya: { name: "Ваня", description: "Опис Вані (Івана, якщо повністю)", photo: "/stylesheets/images/vanya.jpg" },
-    esfer: { name: "Есфер", description: "Опис Еськи", photo: "/stylesheets/images/esfer.jpg" }
+    nazar: { name: "Назар", description: "???", photo: "/stylesheets/images/nazar.jpg" },
+    illya: { name: "Ілля", description: "Самородок", photo: "/stylesheets/images/illya.jpg" },
+    diana: { name: "Діана", description: "Нижній текст", photo: "/stylesheets/images/diana.jpg" },
+    vanya: { name: "Ваня", description: "Maybe it’s all going to be ok…Maybe it won’t🔒", photo: "/stylesheets/images/vanya.jpg" },
+    esfer: { name: "Есфер", description: "Містєр татар", photo: "/stylesheets/images/esfer.jpg" }
 };
 
 router.get('/:name', function(req, res) {
@@ -14,7 +14,7 @@ router.get('/:name', function(req, res) {
     if (!person) {
         return res.status(404).render('error', { message: "Користувача не знайдено" });
     }
-    res.render('about', { person });
+    res.render('about', { person, currentName: req.params.name });
 });
 
 
