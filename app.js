66a9512982b8e72b641a7a4ef3c1bd12f88e2c08
Nazar <nazar.beldyaga@gmail.com>
2025-02-18 09:23:44 +0200

    Error.ejs is written, index.ejs and layout.ejs are schematic.

--- app.js
+++ app.js
@@ -34,13 +34,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title: "Помилка", message: err.message, error: err });
 });
 
 app.listen(PORT, () => console.log("Hello, World!"))
